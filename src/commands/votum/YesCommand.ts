1a93d3819f3009b948ba8fd4299c4c1ff87d9407
Eryn Lynn <eryn@eryn.io>
2018-04-05 20:01:32 -0400

    Add oui

--- src/commands/votum/YesCommand.ts
+++ src/commands/votum/YesCommand.ts
@@ -7,7 +7,7 @@ export default class YesCommand extends VoteAliasCommand {
   constructor (client: CommandoClient) {
     super(client, {
       name: 'yes',
-      aliases: ['aye', 'si', 'yea', 'yay', 'ja'],
+      aliases: ['aye', 'si', 'yea', 'yay', 'ja', 'oui'],
       description: 'Vote yes on a motion',
 
       args: [
