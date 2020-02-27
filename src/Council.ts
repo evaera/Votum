6eac2b9a4a5824b8c080ccc2fc69c3ab56986758
Eryn Lynn <eryn@eryn.io>
2020-02-26 21:38:26 -0500

    Add automatic backups

--- src/Council.ts
+++ src/Council.ts
@@ -1,10 +1,10 @@
-import { Collection, GuildMember, Snowflake, TextChannel } from 'discord.js'
-import * as fs from 'fs'
-import onChange from 'on-change'
-import * as path from 'path'
-import { CouncilData, DefaultCouncilData } from './CouncilData'
-import Motion from './Motion'
-import { MotionData } from './MotionData'
+import { Collection, GuildMember, Snowflake, TextChannel } from "discord.js"
+import * as fs from "fs"
+import onChange from "on-change"
+import * as path from "path"
+import { CouncilData, DefaultCouncilData } from "./CouncilData"
+import Motion from "./Motion"
+import { MotionData } from "./MotionData"
 
 export default class Council {
   private static defaultData = DefaultCouncilData
@@ -14,7 +14,7 @@ export default class Council {
   private data: CouncilData
   private dataPath: string
 
-  constructor (channel: TextChannel) {
+  constructor(channel: TextChannel) {
     this.channel = channel
     this.id = channel.id
 
@@ -22,60 +22,60 @@ export default class Council {
     this.loadData()
   }
 
-  public get enabled () {
+  public get enabled() {
     return this.data.enabled
   }
 
-  public set enabled (state: boolean) {
+  public set enabled(state: boolean) {
     this.data.enabled = state
   }
 
-  public get name () {
+  public get name() {
     return this.data.name
   }
 
-  public set name (state: string) {
+  public set name(state: string) {
     this.data.name = state
   }
 
-  public get announceChannel (): string | undefined {
+  public get announceChannel(): string | undefined {
     return this.data.announceChannel
   }
 
-  public get councilorRole (): Snowflake | undefined {
+  public get councilorRole(): Snowflake | undefined {
     return this.data.councilorRole
   }
 
-  public get userCooldown (): number {
+  public get userCooldown(): number {
     return this.data.userCooldown
   }
 
-  public get motionExpiration (): number {
+  public get motionExpiration(): number {
     return this.data.motionExpiration
   }
 
-  public setConfig<T extends keyof CouncilData> (key: T, value: CouncilData[T]) {
+  public setConfig<T extends keyof CouncilData>(key: T, value: CouncilData[T]) {
     this.data[key] = value
   }
 
-  public getConfig<T extends keyof CouncilData> (key: T): CouncilData[T] {
+  public getConfig<T extends keyof CouncilData>(key: T): CouncilData[T] {
     return this.data[key]
   }
 
-  public get mentionString () {
+  public get mentionString() {
     if (this.data.councilorRole) {
       return `<@&${this.data.councilorRole}>`
     }
 
-    return ''
+    return ""
   }
 
-  public get size (): number {
+  public get size(): number {
     return this.members.size
   }
 
-  public get members (): Collection<Snowflake, GuildMember> {
-    const roleId = this.councilorRole || '0'
+  public get members(): Collection<Snowflake, GuildMember> {
+    const roleId = this.councilorRole || "0"
     const role = this.channel.guild.roles.get(roleId)
 
     if (role) {
@@ -85,7 +85,7 @@ export default class Council {
     }
   }
 
-  public get currentMotion (): Motion | undefined {
+  public get currentMotion(): Motion | undefined {
     for (const [index, motion] of this.data.motions.entries()) {
       if (motion.active) {
         return new Motion(index, motion, this)
@@ -93,11 +93,11 @@ export default class Council {
     }
   }
 
-  public get numMotions (): number {
+  public get numMotions(): number {
     return this.data.motions.length
   }
 
-  public isUserOnCooldown (id: Snowflake): boolean {
+  public isUserOnCooldown(id: Snowflake): boolean {
     if (!this.data.userCooldowns[id]) {
       return false
     }
@@ -109,15 +109,15 @@ export default class Council {
     return false
   }
 
-  public getUserCooldown (id: Snowflake): number {
+  public getUserCooldown(id: Snowflake): number {
     return this.userCooldown - (Date.now() - (this.data.userCooldowns[id] || 0))
   }
 
-  public setUserCooldown (id: Snowflake, time: number = Date.now()): void {
+  public setUserCooldown(id: Snowflake, time: number = Date.now()): void {
     this.data.userCooldowns[id] = time
   }
 
-  public getMotion (id: number): Motion {
+  public getMotion(id: number): Motion {
     const motion = this.data.motions[id]
 
     if (motion == null) {
@@ -127,24 +127,44 @@ export default class Council {
     return new Motion(id, motion, this)
   }
 
-  public createMotion (data: MotionData): Motion {
+  public createMotion(data: MotionData): Motion {
     this.data.motions.push(data)
 
     return new Motion(this.data.motions.length - 1, data, this)
   }
 
-  private loadData (): void {
+  private loadData(useBackup?: boolean): void {
     let data: CouncilData
     try {
-      const parsedSettings = JSON.parse(fs.readFileSync(this.dataPath, { encoding: 'utf8' }))
-      data = Object.assign({}, JSON.parse(JSON.stringify(Council.defaultData)), parsedSettings)
+      const parsedSettings = JSON.parse(
+        fs.readFileSync(this.dataPath + (useBackup ? ".bak" : ""), {
+          encoding: "utf8",
+        })
+      )
+      data = Object.assign(
+        {},
+        JSON.parse(JSON.stringify(Council.defaultData)),
+        parsedSettings
+      )
     } catch (e) {
+      if (!useBackup) {
+        return this.loadData(true)
+      }
+
       data = JSON.parse(JSON.stringify(Council.defaultData))
     }
 
     this.data = onChange(data, () => {
       setTimeout(() => {
-        fs.writeFile(this.dataPath, JSON.stringify(this.data, undefined, 2), () => undefined)
+        if (fs.existsSync(this.dataPath)) {
+          fs.renameSync(this.dataPath, this.dataPath + ".bak")
+        }
+
+        fs.writeFile(
+          this.dataPath,
+          JSON.stringify(this.data, undefined, 2),
+          () => undefined
+        )
       }, 0)
     }) as CouncilData
   }
