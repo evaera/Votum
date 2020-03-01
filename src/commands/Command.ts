16df9fb438e7e6c353936731876ba7a1f75e3ce5
Eryn Lynn <eryn@eryn.io>
2020-03-01 02:40:23 -0500

    Allow propose role to create motions without councilor

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -1,37 +1,46 @@
-import * as Commando from 'discord.js-commando'
-import { Message } from 'discord.js'
-import Votum from '../Votum'
-import Council from '../Council'
+import { Message } from "discord.js"
+import * as Commando from "discord.js-commando"
+import Council from "../Council"
+import { CouncilData } from "../CouncilData"
+import Votum from "../Votum"
 
 interface CustomCommandInfo {
-  name: string,
-  aliases?: string[],
-  description: string,
-  councilOnly?: boolean,
-  adminOnly?: boolean,
+  name: string
+  aliases?: string[]
+  description: string
+  councilOnly?: boolean
+  adminOnly?: boolean
   args?: Commando.ArgumentInfo[]
+  allowWithConfigurableRoles?: Array<keyof CouncilData>
 }
 
 export default class Command extends Commando.Command {
   public councilOnly: boolean
   public adminOnly: boolean
   protected council: Council
+  private customInfo: CustomCommandInfo
 
-  constructor (client: Commando.CommandoClient, customInfo: CustomCommandInfo) {
+  constructor(client: Commando.CommandoClient, customInfo: CustomCommandInfo) {
     const info = customInfo as Commando.CommandInfo
 
-    info.group = 'votum'
+    info.group = "votum"
     info.guildOnly = true
     info.memberName = info.name
     info.argsPromptLimit = 0
 
     super(client, info)
 
-    this.councilOnly = typeof customInfo.councilOnly === 'undefined' ? true : customInfo.councilOnly
-    this.adminOnly = typeof customInfo.adminOnly === 'undefined' ? false : customInfo.adminOnly
+    this.customInfo = customInfo
+
+    this.councilOnly =
+      typeof customInfo.councilOnly === "undefined"
+        ? true
+        : customInfo.councilOnly
+    this.adminOnly =
+      typeof customInfo.adminOnly === "undefined" ? false : customInfo.adminOnly
   }
 
-  public hasPermission (msg: Commando.CommandMessage): boolean {
+  public hasPermission(msg: Commando.CommandMessage): boolean {
     const council = Votum.getCouncil(msg.channel.id)
 
     if (this.client.isOwner(msg.author)) {
@@ -39,7 +48,19 @@ export default class Command extends Commando.Command {
     }
 
     if (this.adminOnly) {
-      return msg.member.hasPermission('MANAGE_GUILD') || !!msg.member.roles.find('name', 'Votum Admin')
+      return (
+        msg.member.hasPermission("MANAGE_GUILD") ||
+        !!msg.member.roles.find("name", "Votum Admin")
+      )
+    } else if (
+      this.council &&
+      this.customInfo.allowWithConfigurableRoles &&
+      this.customInfo.allowWithConfigurableRoles.find(
+        configName =>
+          this.council.getConfig(configName) &&
+          msg.member.roles.has(this.council.getConfig(configName) as string)
+      )
+    ) {
     } else if (council.councilorRole != null) {
       return msg.member.roles.has(council.councilorRole)
     }
@@ -47,17 +68,25 @@ export default class Command extends Commando.Command {
     return true
   }
 
-  public async execute (msg: Commando.CommandMessage, args: any, fromPattern?: boolean): Promise<Message | Message[] | undefined> {
-    return msg.reply('This command has no implementation.')
+  public async execute(
+    msg: Commando.CommandMessage,
+    args: any,
+    fromPattern?: boolean
+  ): Promise<Message | Message[] | undefined> {
+    return msg.reply("This command has no implementation.")
   }
 
-  public async run (msg: Commando.CommandMessage, args: any, fromPattern?: boolean): Promise<Message | Message[]> {
+  public async run(
+    msg: Commando.CommandMessage,
+    args: any,
+    fromPattern?: boolean
+  ): Promise<Message | Message[]> {
     this.council = Votum.getCouncil(msg.channel.id)
 
     const reply = this.execute(msg, args, fromPattern)
 
     if (reply == null) {
-      return msg.reply('Command executed.')
+      return msg.reply("Command executed.")
     }
 
     return reply as Promise<Message | Message[]>
--- src/commands/votum/MotionCommand.ts
+++ src/commands/votum/MotionCommand.ts
@@ -1,40 +1,48 @@
-import { CommandoClient, CommandMessage } from 'discord.js-commando'
-import { Message } from 'discord.js'
-import Command from '../Command'
-import Motion, { LegacyMotionVoteType, MotionResolution } from '../../Motion'
-import { response, ResponseType } from '../../Util'
-import { PathReporter } from 'io-ts/lib/PathReporter'
+import { Message } from "discord.js"
+import { CommandMessage, CommandoClient } from "discord.js-commando"
+import { PathReporter } from "io-ts/lib/PathReporter"
+import Motion, { LegacyMotionVoteType, MotionResolution } from "../../Motion"
+import { response, ResponseType } from "../../Util"
+import Command from "../Command"
 
 export default class MotionCommand extends Command {
-  constructor (client: CommandoClient) {
+  constructor(client: CommandoClient) {
     super(client, {
-      name: 'motion',
-      aliases: ['propose', 'proposal', 'call'],
-      description: 'Create a motion',
+      name: "motion",
+      aliases: ["propose", "proposal", "call"],
+      description: "Create a motion",
+
+      allowWithConfigurableRoles: ["proposeRole"],
 
       args: [
         {
-          key: 'text',
-          prompt: 'The text of the motion to propose.',
-          type: 'string',
-          default: ''
-        }
-      ]
+          key: "text",
+          prompt: "The text of the motion to propose.",
+          type: "string",
+          default: "",
+        },
+      ],
     })
   }
 
-  async execute (msg: CommandMessage, args: any): Promise<Message | Message[]> {
+  async execute(msg: CommandMessage, args: any): Promise<Message | Message[]> {
     if (!args.text) {
       if (this.council.currentMotion) {
         return this.council.currentMotion.postMessage()
       } else {
-        return msg.reply('There is no active motion. Run `!motion <text>` to start one.')
+        return msg.reply(
+          "There is no active motion. Run `!motion <text>` to start one."
+        )
       }
     }
 
     if (this.council.currentMotion) {
-      if (args.text === 'kill') {
-        if (this.council.currentMotion.authorId === msg.author.id || (msg.member.hasPermission('MANAGE_GUILD') || !!msg.member.roles.find('name', 'Votum Admin'))) {
+      if (args.text === "kill") {
+        if (
+          this.council.currentMotion.authorId === msg.author.id ||
+          msg.member.hasPermission("MANAGE_GUILD") ||
+            !!msg.member.roles.find("name", "Votum Admin")
+        ) {
           const motion = this.council.currentMotion
           motion.resolve(MotionResolution.Killed)
           return motion.postMessage()
@@ -43,30 +51,38 @@ export default class MotionCommand extends Command {
         }
       }
 
-      if (!this.council.getConfig('motionQueue')) {
-        return msg.reply('There is already an active motion.')
+      if (!this.council.getConfig("motionQueue")) {
+        return msg.reply("There is already an active motion.")
       }
     }
 
-    if (args.text === 'kill') {
-      return msg.reply('There is no motion active.')
+    if (args.text === "kill") {
+      return msg.reply("There is no motion active.")
     }
 
-    if (this.council.getConfig('councilorMotionDisable')) {
-      return msg.reply('Creating motions is disabled in this council.')
+    if (this.council.getConfig("councilorMotionDisable")) {
+      return msg.reply("Creating motions is disabled in this council.")
     }
 
-    const proposeRole = this.council.getConfig('proposeRole')
+    const proposeRole = this.council.getConfig("proposeRole")
     if (proposeRole && !msg.member.roles.has(proposeRole)) {
       return msg.reply("You don't have permission to propose motions.")
     }
 
     if (args.text.length > 2000) {
-      return msg.reply('Your motion is too long. The maximum length is 2000 characters.')
+      return msg.reply(
+        "Your motion is too long. The maximum length is 2000 characters."
+      )
     }
 
     if (this.council.isUserOnCooldown(msg.author.id)) {
-      return msg.reply(`You must wait ${+(this.council.userCooldown / 3600000).toFixed(2)} hours between motions. (${+(this.council.getUserCooldown(msg.author.id) / 3600000).toFixed(2)} hours left)`)
+      return msg.reply(
+        `You must wait ${+(this.council.userCooldown / 3600000).toFixed(
+          2
+        )} hours between motions. (${+(
+          this.council.getUserCooldown(msg.author.id) / 3600000
+        ).toFixed(2)} hours left)`
+      )
     }
 
     let voteType = LegacyMotionVoteType.Majority
@@ -74,24 +90,28 @@ export default class MotionCommand extends Command {
     const result = Motion.parseMotionOptions(args.text)
 
     if (result.isLeft()) {
-      return msg.reply(response(
-        ResponseType.Bad,
-        PathReporter.report(result).join('\n')
-      ))
+      return msg.reply(
+        response(ResponseType.Bad, PathReporter.report(result).join("\n"))
+      )
     }
 
     const [text, options] = result.value
 
-    if (options.majority && options.majority < this.council.getConfig('majorityMinimum')) {
-      return msg.reply(response(
-        ResponseType.Bad,
-        `The given majority type is disallowed by the ~majority.minimum~ configuration point. Please specify a higher majority.`
-      ))
+    if (
+      options.majority &&
+      options.majority < this.council.getConfig("majorityMinimum")
+    ) {
+      return msg.reply(
+        response(
+          ResponseType.Bad,
+          `The given majority type is disallowed by the ~majority.minimum~ configuration point. Please specify a higher majority.`
+        )
+      )
     }
 
     const motionAlreadyExists = this.council.currentMotion
 
-    if (this.council.getConfig('userCooldownKill')) {
+    if (this.council.getConfig("userCooldownKill")) {
       this.council.setUserCooldown(msg.author.id, Date.now())
     }
 
@@ -105,14 +125,16 @@ export default class MotionCommand extends Command {
       resolution: MotionResolution.Unresolved,
       didExpire: false,
       votes: [],
-      options
+      options,
     })
 
     if (motionAlreadyExists) {
-      return msg.reply(response(
-        ResponseType.Good,
-        'Your motion has been queued and will begin after the current motion.'
-      ))
+      return msg.reply(
+        response(
+          ResponseType.Good,
+          "Your motion has been queued and will begin after the current motion."
+        )
+      )
     }
 
     return motion.postMessage(true)
