2a6c5653452f96695e9542e68a6bf7f5fda8b44f
Spikers <37247310+Sqikerz@users.noreply.github.com>
2023-11-16 13:39:20 -0500

    Update MotionCommand.ts

--- src/commands/votum/MotionCommand.ts
+++ src/commands/votum/MotionCommand.ts
@@ -1,66 +1,36 @@
+import { PieceContext } from "@sapphire/framework"
 import { Message } from "discord.js"
-import { CommandoClient, CommandoMessage } from "discord.js-commando"
 import { PathReporter } from "io-ts/lib/PathReporter"
 import Motion, { LegacyMotionVoteType, MotionResolution } from "../../Motion"
 import { response, ResponseType } from "../../Util"
-import Command from "../Command"
+import ICommand from "../ICommand"
 
-export default class MotionCommand extends Command {
-  constructor(client: CommandoClient) {
+export default class MotionCommand extends ICommand {
+  constructor(client: PieceContext) {
     super(client, {
       name: "motion",
       aliases: ["propose", "proposal", "call"],
       description: "Create a motion",
 
       allowWithConfigurableRoles: ["proposeRole"],
-      adminsAlwaysAllowed: true,
-
-      args: [
-        {
-          key: "text",
-          prompt: "The text of the motion to propose.",
-          type: "string",
-          default: "",
-        },
-      ],
+      adminsAlwaysAllowed: true
     })
   }
 
-  async execute(msg: CommandoMessage, args: any): Promise<Message | Message[]> {
-    await msg.guild.members.fetch() // Privileged intents fix
+  async execute(msg: Message): Promise<Message | Message[]> {
+    let args = msg.toString().split(" ").slice(1).join(" ")
+    await msg.guild?.members.fetch() // Privileged intents fix
 
-    if (!args.text) {
-      if (this.council.currentMotion) {
-        return this.council.currentMotion.postMessage()
-      } else {
-        return msg.reply(
-          "There is no active motion. Run `!motion <text>` to start one."
-        )
-      }
+    if (!args) {
+      return this.executeWithoutArguments(msg);
     }
 
-    if (this.council.currentMotion) {
-      if (args.text === "kill") {
-        if (
-          this.council.currentMotion.authorId === msg.author.id ||
-          msg.member.hasPermission("MANAGE_GUILD") ||
-          !!msg.member.roles.cache.find((role) => role.name === "Votum Admin")
-        ) {
-          const motion = this.council.currentMotion
-          motion.resolve(MotionResolution.Killed)
-          return motion.postMessage()
-        } else {
-          return msg.reply("You don't have permission to kill this motion.")
-        }
-      }
-
-      if (!this.council.getConfig("motionQueue")) {
-        return msg.reply("There is already an active motion.")
-      }
+    if (args === "kill") {
+      return this.killMotion(msg);
     }
 
-    if (args.text === "kill") {
-      return msg.reply("There is no motion active.")
+    if (this.council.currentMotion && !this.council.getConfig("motionQueue")) {
+      return msg.reply("There is already an active motion.")
     }
 
     if (this.council.getConfig("councilorMotionDisable")) {
@@ -68,11 +38,12 @@ export default class MotionCommand extends Command {
     }
 
     const proposeRole = this.council.getConfig("proposeRole")
+    // @ts-ignore
     if (proposeRole && !msg.member.roles.cache.has(proposeRole)) {
       return msg.reply("You don't have permission to propose motions.")
     }
 
-    if (args.text.length > 2000) {
+    if (args.length > 2000) {
       return msg.reply(
         "Your motion is too long. The maximum length is 2000 characters."
       )
@@ -90,12 +61,12 @@ export default class MotionCommand extends Command {
 
     let voteType = LegacyMotionVoteType.Majority
 
-    const result = Motion.parseMotionOptions(args.text)
+    const result = Motion.parseMotionOptions(args)
 
     if (result.isLeft()) {
-      return msg.reply(
-        response(ResponseType.Bad, PathReporter.report(result).join("\n"))
-      )
+      return msg.reply({
+        embeds: [response(ResponseType.Bad, PathReporter.report(result).join("\n")).embed],
+      })
     }
 
     const [text, options] = result.value
@@ -104,12 +75,12 @@ export default class MotionCommand extends Command {
       options.majority &&
       options.majority < this.council.getConfig("majorityMinimum")
     ) {
-      return msg.reply(
-        response(
+      return msg.reply({
+        embeds: [response(
           ResponseType.Bad,
           `The given majority type is disallowed by the ~majority.minimum~ configuration point. Please specify a higher majority.`
-        )
-      )
+        ).embed]
+      })
     }
 
     const motionAlreadyExists = this.council.currentMotion
@@ -121,6 +92,7 @@ export default class MotionCommand extends Command {
     const motion = this.council.createMotion({
       text,
       authorId: msg.author.id,
+      // @ts-ignore
       authorName: msg.member.displayName,
       createdAt: Date.now(),
       voteType,
@@ -132,14 +104,44 @@ export default class MotionCommand extends Command {
     })
 
     if (motionAlreadyExists) {
-      return msg.reply(
-        response(
+      return msg.reply({
+        embeds: [response(
           ResponseType.Good,
           "Your motion has been queued and will begin after the current motion."
-        )
-      )
+        ).embed]
+      })
     }
 
     return motion.postMessage(true)
+  }
+
+  executeWithoutArguments(msg: Message): Promise<Message | Message[]> {
+    if (this.council.currentMotion) {
+      return this.council.currentMotion.postMessage()
+    } else {
+      return msg.reply(
+        "There is no active motion. Run `!motion <text>` to start one."
+      )
+    }
+  }
+
+  killMotion(msg: Message): Promise<Message | Message[]> {
+    if (this.council.currentMotion) {
+      if (
+        this.council.currentMotion.authorId === msg.author.id ||
+        // @ts-ignore
+        msg.member.hasPermission("MANAGE_GUILD") ||
+        // @ts-ignore
+        !!msg.member.roles.cache.find((role) => role.name === "Votum Admin")
+      ) {
+        const motion = this.council.currentMotion
+        motion.resolve(MotionResolution.Killed)
+        return motion.postMessage()
+      } else {
+        return msg.reply("You don't have permission to kill this motion.")
+      }
+    }
+
+    return msg.reply("There is no motion active.")
   }
 }
