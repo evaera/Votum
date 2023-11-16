708e47c1c9f15372451433b83b9c51a0f216568c
Spikers <37247310+Sqikerz@users.noreply.github.com>
2023-11-16 13:40:32 -0500

    Update YesCommand.ts

--- src/commands/votum/YesCommand.ts
+++ src/commands/votum/YesCommand.ts
@@ -1,23 +1,14 @@
-import { CommandoClient } from 'discord.js-commando'
+import { PieceContext } from "@sapphire/framework"
 import VoteAliasCommand from '../abstract/VoteAliasCommand'
 
 export default class YesCommand extends VoteAliasCommand {
   protected state: 1 | 0 | -1 = 1
 
-  constructor (client: CommandoClient) {
+  constructor (client: PieceContext) {
     super(client, {
       name: 'yes',
       aliases: ['aye', 'si', 'yea', 'yay', 'ja', 'oui', 'da', 'да'],
-      description: 'Vote yes on a motion',
-
-      args: [
-        {
-          key: 'reason',
-          prompt: 'The reason for the vote',
-          type: 'string',
-          default: ''
-        }
-      ]
+      description: 'Vote yes on a motion'
     })
   }
 }
